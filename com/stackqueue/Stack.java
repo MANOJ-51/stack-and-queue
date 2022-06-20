1f7d89f3f1678c8b9ebfc441c814eb129c4855dc
MANOJ-51 <m4anoj51@gmail.com>
2022-06-20 20:02:27 +0530

    added: method to create stack and perform stack operations

--- com/stackqueue/Stack.java
+++ com/stackqueue/Stack.java
@@ -1,9 +1,29 @@
 package com.stackqueue;
 
-public class Stack {
+import java.util.LinkedList;
 
+public class Stack {
+    //inititalizizng linked list
+    LinkedList<Integer> linkedList = new LinkedList<Integer>();
+
+    //adding first method
+    public void push(Integer data){
+        linkedList.addFirst(data);
+    }
+
+    //printing list
+    public void printStack(){
+        System.out.println(linkedList.toString());
+    }
 
     public static void main(String[] args) {
         System.out.println("welcome to stack and queue");
+
+        //creating object of stack class
+        Stack stack=new Stack();
+        stack.push(70);
+        stack.push(30);
+        stack.push(56);
+        stack.printStack();
     }
 }
