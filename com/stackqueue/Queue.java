fb65baf1db6119dc851601202bd835973e69538e
MANOJ-51 <m4anoj51@gmail.com>
2022-06-20 20:29:03 +0530

    added: method to dequeue from bigining

--- com/stackqueue/Queue.java
+++ com/stackqueue/Queue.java
@@ -4,25 +4,32 @@ import java.util.LinkedList;
 
 public class Queue {
     //initializing linked list
-    LinkedList <Integer> linkedList =new LinkedList<Integer>();
-
-    //adding enque method
-    public void enqueue(Integer data){
-        linkedList.addLast(data);
-    }
-
-    //printing queue
-    public void printQueue(){
-        System.out.println(linkedList.toString());
-    }
+    LinkedList<Integer> linkedList = new LinkedList<Integer>();
 
     public static void main(String[] args) {
         System.out.println("welcome to queue");
 
-        Queue queue=new Queue();
+        Queue queue = new Queue();
         queue.enqueue(56);
         queue.enqueue(30);
         queue.enqueue(70);
         queue.printQueue();
+        queue.dequeue();
+        queue.printQueue();
+    }
+
+    //adding enque method
+    public void enqueue(Integer data) {
+        linkedList.addLast(data);
+    }
+
+    //adding dequeue method
+    public void dequeue() {
+        linkedList.removeFirst();
+    }
+
+    //printing queue
+    public void printQueue() {
+        System.out.println(linkedList.toString());
     }
 }
